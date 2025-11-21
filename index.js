28450526995479c8e878bb337a0a2f5f72211195
Shubham Gupta <guptashubahm0324@gmail.com>
2025-11-22 00:10:09 +0530

    Add middleware for JWT auth

--- index.js
+++ index.js
@@ -1,81 +1,84 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "shubhamgupta2403";
 const app = express();
 
 app.use(express.json());
 
 const users = [];
 
-function generateToken() {
-  const options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
+// function generateToken() {
+//   const options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
+//   let token = "";
+//   for (let i = 0; i < 32; i++) {
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// }
 
 app.post("/signup", function (req, res) {
   const username = req.body.username;
@@ -110,8 +113,13 @@ app.post("/signin", function (req, res) {
   });
 
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token;
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      JWT_SECRET
+    ); //convert {username into jwt token}
+    // foundUser.token = token;
 
     res.json({
       token: token,
@@ -126,8 +134,12 @@ app.post("/signin", function (req, res) {
 
 app.get("/me", function (req, res) {
   const token = req.headers.token;
+
+  const decodeInformation = jwt.verify(token, JWT_SECRET); //(converts {username: "sgupta4850@gmail.com"})
+  const username = decodeInformation.username;
+
   let foundUser = users.find((u) => {
-    if (u.token == token) {
+    if (u.username == username) {
       return true;
     } else {
       false;
